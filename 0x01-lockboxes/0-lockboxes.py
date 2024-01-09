d175f7f4c59d08a78f8915c7795f87e92690c54f
alidrisy <a.alidrisy6@gmail.com>
2024-01-09 22:10:09 +0300

    Do not open close boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,11 +6,14 @@ def canUnlockAll(boxes):
     """ Determines if all the boxes can be opened. """
     if len(boxes[0]) == 0:
         return False
+    boxes[0].append(True)
     for i in range(len(boxes)):
-        for k in boxes[i]:
-            if k != i and True not in boxes[k] and type(k) is int:
-                boxes[k].append(True)
-    for i in range(1, len(boxes)):
+        if True in boxes[i]:
+            for k in boxes[i]:
+                if k < len(boxes) and k != i and True not in boxes[k] and\
+                        type(k) is int:
+                    boxes[k].append(True)
+    for i in range(len(boxes)):
         if True not in boxes[i]:
             return False
     return True
