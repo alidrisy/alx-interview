9411e413820181577b1c74ee90ed5c15b35f3b7c
alidrisy <a.alidrisy6@gmail.com>
2024-01-25 20:29:02 +0300

    try to fix errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,31 +3,34 @@
 from sys import stdin
 import re
 
+
 pattern =\
     r"^(?P<ip_addrees>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(?P<date>.*?)\] "
 r"\"GET /projects/260 HTTP/1.1\" "
 r"(?P<status_code>\d{3}) (?P<file_size>\d+)$"
 
-status = ["200", "301", "400", "401", "403", "404", "405", "500"]
-i = 0
-dict_autput = {}
-file_size = 0
-try:
-    for line in stdin:
-        match = re.match(pattern, line)
-        if match:
-            file_size += int(line.split()[-1])
-            statu = line.split()[-2]
-            if statu in dict_autput:
-                dict_autput[status] += 1
-            elif statu in ststus:
-                dict_autput[status] = 1
-            i += 1
-            if i % 10 == 0:
-                print(f"File size: {file_size}")
-                for k, v in sorted(dict_autput.items()):
-                    print(f"{k}: {v}")
-finally:
-    print(f"File size: {file_size}")
-    for k, v in sorted(dict_autput.items()):
-        print(f"{k}: {v}")
+
+if __name__ == '__main__':
+    status = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    i = 0
+    dict_autput = {}
+    file_size = 0
+    try:
+        for line in stdin:
+            match = re.match(pattern, line)
+            if match:
+                file_size += int(line.split()[-1])
+                statu = line.split()[-2]
+                if statu in dict_autput:
+                    dict_autput[statu] += 1
+                elif statu in status:
+                    dict_autput[statu] = 1
+                i += 1
+                if i % 10 == 0:
+                    print(f"File size: {file_size}")
+                    for k, v in sorted(dict_autput.items()):
+                        print(f"{k}: {v}")
+    finally:
+        print(f"File size: {file_size}")
+        for k, v in sorted(dict_autput.items()):
+            print(f"{k}: {v}")
