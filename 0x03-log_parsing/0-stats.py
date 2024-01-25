ce19c53f7ddc55564f30dd5cadd7a90eece0aa10
alidrisy <a.alidrisy6@gmail.com>
2024-01-25 21:58:31 +0300

    fix errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,29 +4,35 @@ from sys import stdin
 import re
 
 
+patern = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+status = ["200", "301", "400", "401", "403", "404", "405", "500"]
+i = 0
+dict_autput = {}
+file_size = 0
+
+
+def print_all():
+    print("File size: {}".format(file_size))
+    for k, v in sorted(dict_autput.items()):
+        print("{}: {}".format(k, v))
+
+
 if __name__ == '__main__':
-    patern = re.compile(
-            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-    status = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    i = 0
-    dict_autput = {}
-    file_size = 0
     try:
         for line in stdin:
             line = line.strip()
             if (patern.fullmatch(line)):
+                if (line.split()[-1]).isdigit():
                 file_size += int(line.split()[-1])
                 statu = line.split()[-2]
                 if statu in dict_autput:
                     dict_autput[statu] += 1
                 elif statu.isdigit() and statu in status:
                     dict_autput[statu] = 1
-                i += 1
-                if i % 10 == 0:
-                    print("File size: {}".format(file_size))
-                    for k, v in sorted(dict_autput.items()):
-                        print("{}: {}".format(k, v))
+            i += 1
+            if i % 10 == 0:
+                print_all()
     finally:
-        print("File size: {}".format(file_size))
-        for k, v in sorted(dict_autput.items()):
-            print("{}: {}".format(k, v))
+        print_all()
