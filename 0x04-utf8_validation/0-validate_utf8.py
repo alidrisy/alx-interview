ff589b6a80394ca35da4b570359afe4770d556de
alidrisy <a.alidrisy6@gmail.com>
2024-01-30 21:19:24 +0300

    0. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Model for validUTF8 function """
+
+
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding. """
+    if all(i < 128 for i in data):
+        return True
+    return False
