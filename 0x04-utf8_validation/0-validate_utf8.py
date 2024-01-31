926e3eafb75a5c67f0707301ab1b38be1cdf050f
alidrisy <a.alidrisy6@gmail.com>
2024-01-31 21:52:11 +0300

    use a mask way

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,22 +4,48 @@
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding."""
+    masks = [128, 64, 32, 16, 8]
 
-    count = 0
+    def get_type(num: int) -> int:
+        for i in range(5):
+            if num & masks[i] == 0:
+                return i
+        return -1
 
-    for i in data:
-        if count == 0:
-            if (i >> 5) == 0b110:
-                count = 1
-            elif (i >> 4) == 0b1110:
-                count = 2
-            elif (i >> 3) == 0b11110:
-                count = 3
-            elif (i >> 7) != 0:
-                return False
+    len_data = len(data)
+    i = 0
+
+    while i < len_data:
+        curr = data[i]
+        type = get_type(curr)
+
+        if type == 0:
+            i += 1
+        elif type > 1 and i + type <= len_data:
+            while type > 1:
+                i += 1
+                if get_type(data[i]) != 1:
+                    return False
+                type -= 1
+            i += 1
         else:
-            if (i >> 6) != 0b10:
-                return False
-            count -= 1
+            return False
+    return True
+    # count = 0
 
-    return count == 0
+    # for i in data:
+    #     if count == 0:
+    #         if (i >> 5) == 0b110:
+    #             count = 1
+    #         elif (i >> 4) == 0b1110:
+    #             count = 2
+    #         elif (i >> 3) == 0b11110:
+    #             count = 3
+    #         elif (i >> 7) != 0:
+    #             return False
+    #     else:
+    #         if (i >> 6) != 0b10:
+    #             return False
+    #         count -= 1
+
+    # return count == 0
