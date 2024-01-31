a1432b05b242ae4ce970940841fe5f7019010f00
alidrisy <a.alidrisy6@gmail.com>
2024-01-31 21:38:05 +0300

    use a mask way

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,23 @@
 
 
 def validUTF8(data):
-    """ determines if a given data set represents a valid UTF-8 encoding. """
-    if all(i < 256 for i in data):
-        return True
-    return False
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+
+    count = 0
+
+    for i in data:
+        if count == 0:
+            if (i >> 5) == 0b110:
+                count = 1
+            elif (i >> 4) == 0b1110:
+                count = 2
+            elif (i >> 3) == 0b11110:
+                count = 2
+            elif (i >> 7) != 0:
+                return False
+        else:
+            if (i >> 6) != 0b10:
+                return False
+            count -= 1
+
+    return count == 0
