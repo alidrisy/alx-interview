34bf6c15d34a3515057164b59a511ae11774d332
alidrisy <a.alidrisy6@gmail.com>
2024-02-01 18:16:14 +0300

    three ways to solve one problem

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,7 @@
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding.
         there is three ways to solve this problem check 1-validate_utf8.py and
-        2-validate_utf8.py to see the other ways .. but this way is the most 
+        2-validate_utf8.py to see the other ways .. but this way is the most
         save and effective way.
     """
 
