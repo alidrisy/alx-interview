f5aafdd8f0e2079fa732a1c6f41bfad70ed4832c
alidrisy <a.alidrisy6@gmail.com>
2024-02-13 18:06:42 +0300

    0. Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+// prints all characters of a Star Wars movie
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+function charRequest (characters, num) {
+  if (num >= characters.length) return;
+
+  request.get(characters[num], (_req, resp) => {
+    const charBody = JSON.parse(resp.body);
+    const charName = charBody.name;
+    console.log(charName);
+    charRequest(characters, num + 1);
+  });
+}
+
+request.get(url, (_req, resp) => {
+  if (resp) {
+    const body = JSON.parse(resp.body);
+    const characters = body.characters;
+    console.log(characters);
+    charRequest(characters, 0);
+  }
+});
