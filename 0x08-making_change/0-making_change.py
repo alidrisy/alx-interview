124774ba062de93f709da2508ac7e20f363b7807
alidrisy <a.alidrisy6@gmail.com>
2024-03-02 19:03:53 +0300

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,5 @@
 #!/usr/bin/python3
 """ Model for makingChange """
-import sys
-
-
-def makeChangeUtil(coins, m, total, dp):
-    """determine the fewest number of coins needed to meet
-    a given amount total"""
-    if total == 0:
-        return 0
-
-    if dp[total] != -1:
-        return dp[total]
-
-    res = sys.maxsize
-
-    for i in range(m):
-        if coins[i] <= total:
-            sub_res = makeChangeUtil(coins, m, total - coins[i], dp)
-
-            if sub_res != sys.maxsize and sub_res + 1 < res:
-                res = sub_res + 1
-    dp[total] = res
-
-    return res
 
 
 def makeChange(coins, total):
@@ -30,9 +7,19 @@ def makeChange(coins, total):
     a given amount total"""
     if total <= 0:
         return 0
-    n = len(coins)
-    dp = [-1] * (total + 1)
-    res = makeChangeUtil(coins, n, total, dp)
-    if res == sys.maxsize:
-        res = -1
-    return res
+
+    count = 0
+
+    coins.sort(reverse=True)
+    print(coins)
+    for coin in coins:
+        if total <= 0:
+            break
+
+        count = count + (total // coin)
+        total = total % coin
+
+    if total == 0:
+        return count
+    else:
+        return -1
