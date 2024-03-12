d7eccf7363c2efddab3e3fd57a32c731e52e5137
alidrisy <a.alidrisy6@gmail.com>
2024-03-12 23:58:12 +0300

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,7 +8,7 @@ def SieveOfEratosthenes(n):
     p = 2
     while p * p <= n:
 
-        if bool_prime[p] == True:
+        if bool_prime[p]:
 
             for i in range(p * p, n + 1, p):
                 bool_prime[i] = False
@@ -30,7 +30,6 @@ def isWinner(x, nums):
     while x > 0:
 
         prime = SieveOfEratosthenes(nums[i])
-        n = len(prime) - 1
 
         if len(prime) == 1 and i == len(nums) - 1:
             if round == "maria":
@@ -40,18 +39,17 @@ def isWinner(x, nums):
                 round = "maria"
                 ben += 1
 
+        n = len(prime) - 1
         for y in range(n, 0, -1):
             if round == "maria":
                 round = "ben"
                 if y == 1:
                     maria += 1
-                    print("maria")
                 continue
             elif round == "ben":
                 round = "maria"
                 if y == 1:
                     ben += 1
-                    print("ben")
                 continue
         x -= 1
         i += 1
